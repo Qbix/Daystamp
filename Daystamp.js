08cba2d05b8ea0380a56170940fde972a2b06f72
Greg Magarshak <greg@qbix.com>
2022-12-31 22:01:28 -0500

    Implemented Daystamp.js and Daystamp.php

---
+++ Daystamp.js
@@ -0,0 +1,68 @@
+/**
+ * Like a timestamp, but works with number of Gregorian Calendar 
+ * days since fictional epoch year=0, month=0, day=1.
+ * You can store daystamps and do arithmetic with them.
+ * @class Daystamp
+ */
+Daystamp = {
+	fromTimestamp: function (timestamp) {
+		return Math.round(
+			(timestamp - Daystamp.epoch) / Daystamp.msPerDay
+		);
+	},
+	fromDate: function (date) {
+		return Daystamp.fromTimestamp(date.getTime());
+	},
+	fromDateTime: function (datetime) {
+		var time = Date.parse(datetime + ' UTC');
+		return Math.round(
+			(new Date(time).getTime() - Daystamp.epoch) / Daystamp.msPerDay
+		);
+	},
+	fromYMD: function (y, m, d) {
+		var date = new Date();
+		date.setUTCFullYear(y, m+1, d);
+		date.setUTCHours(0, 0, 0);
+		return Math.round(
+			(date.getTime() - Daystamp.epoch) / Daystamp.msPerDay
+		);
+	},
+	toTimestamp: function (daystamp) {
+		return Daystamp.epoch + Daystamp.msPerDay * daystamp;
+	},
+	toDate: function (daystamp) {
+		return new Date(Daystamp.toTimestamp(daystamp));
+	},
+	toDateTime(daystamp, separator) {
+		var date = Daystamp.toDate(daystamp);
+		if (separator === undefined) {
+			separator = ' ';
+		}
+		return String(date.getUTCFullYear()).padStart(4, 0)
+			+ '-' + String(date.getUTCMonth()+1).padStart(2, 0)
+			+ '-' + String(date.getUTCDate()).padStart(2, 0)
+			+ separator + '00:00:00';
+	},
+	toYMD: function (daystamp) {
+		var date = Daystamp.toDate(daystamp);
+		return [
+			date.getUTCFullYear(),
+			date.getUTCMonth(),
+			date.getUTCDate()
+		];
+	}
+};
+
+Object.defineProperty(Daystamp, 'epoch', {
+	value: -62167219200000,
+	configurable: false,
+	writable: false,
+	enumerable: true
+});
+
+Object.defineProperty(Daystamp, 'msPerDay', {
+	value: 8.64e7,
+	configurable: false,
+	writable: false,
+	enumerable: true
+});
