9d367b03299127a27f39414020600bf46740a8e9
Greg Magarshak <greg@qbix.com>
2022-12-31 22:20:02 -0500

    Added DocBlock comments in PHP and JS

--- Daystamp.js
+++ Daystamp.js
@@ -10,43 +10,45 @@ const Daystamp = {
      * @method fromTimestamp
      * @static
      * @param {Number} timestamp 
-     * @returns {Number}
+     * @return {Number}
      */
 	fromTimestamp: function (timestamp) {
 		return Math.round(
 			(timestamp - Daystamp.epoch) / Daystamp.msPerDay
 		);
 	},
+
     /**
      * Get daystamp from a Javascript Date object
      * @method fromDate
      * @static
      * @param {Date} date 
-     * @returns {Number}
+     * @return {Number}
      */
 	fromDate: function (date) {
 		return Daystamp.fromTimestamp(date.getTime());
 	},
+
     /**
      * Get daystamp from a string of the form "yyyy-mm-dd"
      * or "yyyy-mm-dd hh:mm:ss"
      * @method fromDateTime
      * @static
      * @param {String} datetime 
-     * @returns {Number}
+     * @return {Number}
      */
 	fromDateTime: function (datetime) {
 		return this.fromTimestamp(Date.parse(datetime + ' UTC'));
 	},
+
     /**
-     * Get daystamp from a string of the form "yyyy-mm-dd"
-     * or "yyyy-mm-dd hh:mm:ss"
+     * Get daystamp from a year, month and day
      * @method fromYMD
      * @static
      * @param {Number} year 
      * @param {Number} month January is 1
      * @param {Number} day
-     * @returns {Number}
+     * @return {Number}
      */
 	fromYMD: function (year, month, day) {
 		const date = new Date();
@@ -56,32 +58,35 @@ const Daystamp = {
 			(date.getTime() - Daystamp.epoch) / Daystamp.msPerDay
 		);
 	},
+
     /**
      * Get Javascript milliseconds-based timestamp from a daystamp
      * @method toTimestamp
      * @static
      * @param {Number} daystamp 
-     * @returns {Number}
+     * @return {Number}
      */
 	toTimestamp: function (daystamp) {
 		return Daystamp.epoch + Daystamp.msPerDay * daystamp;
 	},
+
     /**
      * Get Javascript Date from a daystamp
      * @method toDate
      * @static
      * @param {Number} daystamp 
-     * @returns {Date}
+     * @return {Date}
      */
 	toDate: function (daystamp) {
 		return new Date(Daystamp.toTimestamp(daystamp));
 	},
+
     /**
      * Get date-time string from a daystamp
      * @method toDateTime
      * @static
      * @param {Number} daystamp 
-     * @returns {String} String of the form "yyyy-mm-dd 00:00:00"
+     * @return {String} String of the form "yyyy-mm-dd 00:00:00"
      */
 	toDateTime(daystamp, separator) {
 		const date = Daystamp.toDate(daystamp);
@@ -93,12 +98,13 @@ const Daystamp = {
 			+ '-' + String(date.getUTCDate()).padStart(2, 0)
 			+ separator + '00:00:00';
 	},
+
     /**
      * Get Javascript milliseconds-based timestamp from a daystamp
      * @method toYMD
      * @static
      * @param {Number} daystamp 
-     * @returns {Array} [year, month, date] with month, January is 1
+     * @return {Array} [year, month, date] note January is month 1
      */
 	toYMD: function (daystamp) {
 		const date = Daystamp.toDate(daystamp);
@@ -110,6 +116,11 @@ const Daystamp = {
 	}
 };
 
+/**
+ * The daystamp epoch as a timestamp
+ * @property epoch
+ * @static
+ */
 Object.defineProperty(Daystamp, 'epoch', {
 	value: -62167219200000,
 	configurable: false,
@@ -117,6 +128,11 @@ Object.defineProperty(Daystamp, 'epoch', {
 	enumerable: true
 });
 
+/**
+ * Number of milliseconds in a day
+ * @property msPerDay
+ * @static
+ */
 Object.defineProperty(Daystamp, 'msPerDay', {
 	value: 8.64e7,
 	configurable: false,
